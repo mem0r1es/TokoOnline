05ba50030a7799ced30ba71444c583ec9794155f
hiphyu <135961582+hiphyu@users.noreply.github.com>
2025-07-03 22:14:50 +0700

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,18 +3,22 @@ import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/dashboard.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Setup Supabase
-  await Supabase.initialize(
-    url: 'https://mccdwczueketpqlbobyw.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jY2R3Y3p1ZWtldHBxbGJvYnl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyOTEwMTcsImV4cCI6MjA2Njg2NzAxN30.LAQPMZgZvIUtRVjzkLFh-wXO_RiP9IuwZ3kgSpYghqE',
-  );
-
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   // Setup Supabase
+//   await Supabase.initialize(
+//     url: 'https://mccdwczueketpqlbobyw.supabase.co',
+//     anonKey:
+//         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jY2R3Y3p1ZWtldHBxbGJvYnl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyOTEwMTcsImV4cCI6MjA2Njg2NzAxN30.LAQPMZgZvIUtRVjzkLFh-wXO_RiP9IuwZ3kgSpYghqE',
+//   );
+
+//   runApp(MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
