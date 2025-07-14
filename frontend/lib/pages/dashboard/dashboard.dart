32fe101d19ef763ba4823358ea0424b3cd28c37a
hiphyu <135961582+hiphyu@users.noreply.github.com>
2025-07-15 02:39:51 +0700

    Update dashboard.dart

--- frontend/lib/pages/dashboard/dashboard.dart
+++ frontend/lib/pages/dashboard/dashboard.dart
@@ -1,49 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web/controllers/scroll_controller_manager.dart';
+import 'package:flutter_web/controllers/scroll_controller.dart';
+import 'package:get/get.dart';
+// import 'package:flutter_web/services/product_service.dart';
+// import 'package:flutter_web/widgets/billing.dart';
 import '../../widgets/header_bar.dart';
-import 'isi.dart';
+import 'isi.dart' ;
 import '../shop/our_product.dart';
 import '../about/about_page.dart';
 import '../contact/contact_page.dart';
 // import '../widgets/billing.dart';// Pastikan path ini sesuai dengan struktur proyek And
-import 'package:get/get.dart';
+// import 'package:get/get.dart';
 
-class DashboardPage extends StatefulWidget {
-  const DashboardPage({super.key});
+class HomePage extends GetView<CustomScrollController> {
+  static final String TAG = '/';
 
-  @override
-  State<DashboardPage> createState() => _DashboardPageState();
-}
-
-class _DashboardPageState extends State<DashboardPage> {
-  final String scrollKey = 'dashboard_scroll';
-  late ScrollController _scrollController;
-  late ScrollControllerManager scrollManager; 
-
-  @override
-  void initState() {
-    super.initState();
-    scrollManager = Get.find<ScrollControllerManager>();
-    _scrollController = ScrollController(
-      initialScrollOffset: scrollManager.getOffset(scrollKey),
-    );
-    _scrollController.addListener(() {
-      scrollManager.saveOffset(scrollKey, _scrollController.offset);
-    });
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+      // final scrollController = Get.find<CustomScrollController>().getController('home_scroll');
     return Scaffold(
       backgroundColor: Colors.white,
       body: ListView(
-        controller: _scrollController,
+        controller: controller.scrollController,
         children: [
           const HeaderPages(),
           const Isi(),
@@ -54,7 +33,7 @@ class _DashboardPageState extends State<DashboardPage> {
           // FOOTER: About dan Contact di bawah
           Container(
             padding: const EdgeInsets.all(20),
-            color: const Color(0xFFF8F4FF),
+            color: Color(0xFFF8F4FF),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -70,3 +49,4 @@ class _DashboardPageState extends State<DashboardPage> {
     );
   }
 }
+
