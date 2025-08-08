7bacf3d066cbb1b4582a4b0f17c8b261a5ddd148
hiphyu <stephany@students.usu.ac.id>
2025-08-08 22:14:24 +0700

    update

--- lib/pages/history/history.dart
+++ lib/pages/history/history.dart
@@ -1,10 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_web/controllers/scroll_controller.dart';
-<<<<<<< Updated upstream
-=======
 import 'package:flutter_web/models/order_history_item.dart';
->>>>>>> Stashed changes
 import 'package:flutter_web/pages/history/order_tile.dart';
 import 'package:flutter_web/pages/homepage/home_page.dart';
 import 'package:flutter_web/services/checkout_service.dart';
